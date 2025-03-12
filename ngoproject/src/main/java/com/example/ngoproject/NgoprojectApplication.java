e7c18b4fca6086b758f9a6dd32ecda49c121d008
umama <umamabedrekar@gmail.com>
2025-03-12 10:41:08 +0530

    Initial commit

---
+++ ngoproject/src/main/java/com/example/ngoproject/NgoprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.ngoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NgoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoprojectApplication.class, args);
+	}
+
+}
---
+++ ngoproject/src/test/java/com/example/ngoproject/NgoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
